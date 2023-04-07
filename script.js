fa7c1f02de1729e48291795119d84e33a5a4449b
Hokkyokukou <hokkyokukou27@gmail.com>
2023-04-07 12:30:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var firstColor = document.getElementById('color1');
 var secondColor = document.getElementById('color2');
 var gradientGenerate = document.getElementsByTagName('h3')[0];
 var body = document.getElementById('gradient');
+var randomButton = document.querySelector(".random");
 
 function changeBackground() {
     body.style.background = 
@@ -10,11 +11,26 @@ function changeBackground() {
     + "," 
     + secondColor.value 
     + ")";
-
     gradientGenerate.textContent = body.style.background +";";
+}
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function setRandomGradient() {
+    firstColor.value = getRandomColor();
+    secondColor.value = getRandomColor();
+    changeBackground();
 }
 
 firstColor.addEventListener("input", changeBackground);
 
 secondColor.addEventListener("input", changeBackground);
 
+randomButton.addEventListener("click", setRandomGradient);
