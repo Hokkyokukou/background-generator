41faa418a2a55abded3e1be7503f3e505422e0ce
Hokkyokukou <hokkyokukou27@gmail.com>
2023-04-12 20:16:27 +0200

    direction gradient

--- script.js
+++ script.js
@@ -10,12 +10,15 @@ function myBgGenerator() {
     let randomDirection = document.querySelector('.randomDirection');
 
     function changeBackground() {
+        const direction = actions[linearDirection.value];
         body.style.background = 
-        'linear-gradient( to right, '
-        + firstColor.value 
-        + ',' 
-        + secondColor.value 
-        + ')';
+            'linear-gradient('
+            + direction
+            + ', '
+            + firstColor.value 
+            + ',' 
+            + secondColor.value 
+            + ')';
         gradientGenerate.textContent = body.style.background +';';
     } 
 
@@ -54,37 +57,20 @@ function myBgGenerator() {
         "to right top": "to right top",
         "to left top": "to left top"
     };
-
-    function setDirection() {
-        const direction = actions[linearDirection.value];
-        if (direction) {
-            body.style.background = 
-            'linear-gradient('
-            + direction
-            + ', '
-            + firstColor.value 
-            + ',' 
-            + secondColor.value 
-            + ')';
-        gradientGenerate.textContent = body.style.background +';';
-        } else {
-            console.log("Invalid Input");
-        }
-    } 
-
+    
     const directions = ["to top", "to right", "to bottom", "to left","to right bottom","to left bottom","to right top","to left top"];
 
     function setRandomDirection() {
         let randomNum = Math.floor(Math.random() * directions.length);
         linearDirection.value = actions[directions[randomNum]];
-        setDirection();
+        changeBackground();
     }
 
     window.addEventListener('load', getRandomColor);
     window.addEventListener('load', setRandomGradient);
     window.addEventListener('load', setOnlyFirstRandomGradient);
     window.addEventListener('load', setOnlySecondRandomGradient);
-    window.addEventListener('load', setDirection);
+    window.addEventListener('load', changeBackground);
     window.addEventListener('load', setRandomDirection);
 
     firstColor.addEventListener('input', changeBackground);
@@ -92,7 +78,7 @@ function myBgGenerator() {
     randomButton.addEventListener('click', setRandomGradient);
     firstRandomBtn.addEventListener('click', setOnlyFirstRandomGradient);
     secondRandomBtn.addEventListener('click', setOnlySecondRandomGradient);
-    linearDirection.addEventListener('click', setDirection);
+    linearDirection.addEventListener('click', changeBackground);
     randomDirection.addEventListener('click', setRandomDirection);
 }
 
