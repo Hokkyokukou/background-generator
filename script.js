c08d34db1f1d7ce664fc95b8846bd14030b14d4e
Hokkyokukou <hokkyokukou27@gmail.com>
2023-04-13 20:01:13 +0200

    refactoring

--- script.js
+++ script.js
@@ -6,39 +6,19 @@ function myBgGenerator() {
     const randomButton = document.querySelector('.random');
     const firstRandomBtn = document.querySelector('.firstRandomBtn');
     const secondRandomBtn = document.querySelector('.secondRandomBtn');
-    const linearDirection = document.getElementsByName('toDirection')[0];
+    const gradientDirection = document.getElementsByName('toDirection')[0];
     const randomDirection = document.querySelector('.randomDirection');
-    
-    const actions = {
-        "to right": "to right",
-        "to left": "to left",
-        "to top": "to top",
-        "to bottom": "to bottom",
-        "to right bottom": "to right bottom",
-        "to left bottom": "to left bottom",
-        "to right top": "to right top",
-        "to left top": "to left top",
-        "radial": "radial"
-    };
 
     function changeBackground() {
-        if (linearDirection.value === 'radial') {
-            body.style.background = 
-            `radial-gradient(${firstColor.value} , ${secondColor.value})`;
+        if (gradientDirection.value === 'radial') {
+            body.style.background = `radial-gradient(${firstColor.value}, 
+            ${secondColor.value})`;
             gradientGenerate.textContent = `${body.style.background};`;
         } else {
-            const direction = actions[linearDirection.value];
-            body.style.background = 
-            'linear-gradient('
-            + direction
-            + ', '
-            + firstColor.value 
-            + ',' 
-            + secondColor.value 
-            + ')';
-        gradientGenerate.textContent = body.style.background +';';
+            body.style.background = `linear-gradient(${gradientDirection.value},
+            ${firstColor.value}, ${secondColor.value})`;
+            gradientGenerate.textContent = `${body.style.background};`;
         }
-        
     } 
 
     function getRandomColor() {
@@ -67,8 +47,8 @@ function myBgGenerator() {
     }
 
     function setRandomDirection() {
-        const randomNum = Math.floor(Math.random() * linearDirection.options.length);
-        linearDirection.value = linearDirection.options[randomNum].value;
+        const randomNum = Math.floor(Math.random() * gradientDirection.options.length);
+        gradientDirection.value = gradientDirection.options[randomNum].value;
         changeBackground();
     }
 
@@ -88,7 +68,7 @@ function myBgGenerator() {
     randomButton.addEventListener('click', setRandomGradient);
     firstRandomBtn.addEventListener('click', setOnlyFirstRandomGradient);
     secondRandomBtn.addEventListener('click', setOnlySecondRandomGradient);
-    linearDirection.addEventListener('click', changeBackground);
+    gradientDirection.addEventListener('click', changeBackground);
     randomDirection.addEventListener('click', setRandomDirection);
 }
 
